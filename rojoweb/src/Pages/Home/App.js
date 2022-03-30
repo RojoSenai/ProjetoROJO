4895d8fb32c2d85c5cb5415fa8753893cc1e135c
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-03-30 16:38:27 -0300

    começo reacr

--- rojoweb/src/Pages/Home/App.js
+++ rojoweb/src/Pages/Home/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <main>
-      <header className="App-header">
-        <h1>aaaa</h1>
-      </header>
-      </main>
-    </div>
-  );
+
+export default function Login() {
+  const [Email, setEmail] = useState('');
+  const [Senha, setSenha] = useState('');
+  const [MensagemErro, setMensagemErro] = useState('');
+  const [Islogind, setIslogind] = useState(false);
+
+
+
+  function App() {
+    return (
+      <div className="App">
+        <main>
+          <section className="app-header">
+            <h1>aaaa</h1>
+          </section>
+        </main>
+      </div>
+    )
+  }
 }
-
-export default App;
