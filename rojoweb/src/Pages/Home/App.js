eeed5dd1ee69927d7ca43c5e304765f0074496d3
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-03-30 16:24:48 -0300

    testes

--- rojoweb/src/Pages/Home/App.js
+++ rojoweb/src/Pages/Home/App.js
@@ -3,9 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <main>
       <header className="App-header">
         <h1>aaaa</h1>
       </header>
+      </main>
     </div>
   );
 }
