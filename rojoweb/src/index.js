84cf7eac28f10207d9c4675bee1552c8d4e436a5
Fábio Quatrochi <quatrochifabio145@gmail.com>
2022-04-03 01:40:39 -0300

    Not-Found Criado

--- rojoweb/src/index.js
+++ rojoweb/src/index.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './Pages/Home/App';
-import EmpresaCor from './Pages/EmpresaCor/EmpresaCor.Js'
+
+//import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+//css
+import './index.css';
+
+
+//Imports de paginas
+import NotFound from './Pages/NotFound/NotFound';
+//import Home from './Pages/Home/App';
+// import EmpresaCor from './Pages/EmpresaCor/EmpresaCor.Jsx'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <EmpresaCor />
+    <NotFound />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+// const routing = (
+//   <Router>
+//     <div>
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route path="/Empresacor" component={EmpresaCor} />
+//         <Route path="/notfound" component={NotFound} />
+//         <Redirect to="/" />
+//       </Switch>
+//     </div>
+//   </Router>
+// )
+
+
+
+
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
