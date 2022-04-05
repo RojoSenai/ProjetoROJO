9dae54917b2c09dc30f334a698f789f3a2dfb899
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-04-05 16:20:54 -0300

    logout

--- rojoweb/src/components/Header/Header.jsx
+++ rojoweb/src/components/Header/Header.jsx
@@ -4,15 +4,18 @@ import LetraRojo from '../../assets/img/letter.svg';
 import '../../assets/css/CadastroUsuarioEmpresa.css';
 import { Link } from 'react-router-dom';
 
-//logout
-// logout = () => {
-//     localStorage.removeItem('UsuarioTk');
-//     console.log('Feito o logout');
-// }
 
 
 
 class Cabeca extends Component {
+
+    //logout
+    logout = () => {
+        localStorage.removeItem('UsuarioTk');
+        console.log('Feito o logout');
+    }
+
+
 
     render() {
 
@@ -21,11 +24,12 @@ class Cabeca extends Component {
                 <div>
                     <img className="Rojinho" src={Rojologo} alt="Logo da empresa" />
                 </div>
-                    <div className='ContainerLetras'>
-                    <Link to="/"><a href="" className="Names_a">Eventos</a></Link>  
+                <div className='ContainerLetras'>
+                    <Link to="/"><a href="" className="Names_a">Eventos</a></Link>
                     <Link to="/CadastrarUserAdm"><a href="" className="Names_a">Cadastrar  Usuario</a></Link>
                     <Link to="/CadastrarEmpresa"><a href="" className="Names_a">Cadastrar empresa</a></Link>
-                    </div>
+                    <Link to="/"><a onClick={this.logout} className="Names_a">Sair</a></Link>
+                </div>
             </div>
         )
     }
