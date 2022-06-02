d63db2278da5ada7a5b4dc6c10310f6ccc880540
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-06-02 15:02:12 -0300

    CorEmprresa

---
+++ API/RojoApi/Interfaces/ICorEmpresaRepository.cs
@@ -0,0 +1,17 @@
+﻿using RojoAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RojoAPI.Interfaces
+{
+    interface ICorEmpresaRepository
+    {
+        void Cadastrar(CorEmpresa NovaCor);
+        CorEmpresa BuscarPorId(int id);
+        void Atualizar(int id, CorEmpresa CorAtualizada);
+        void Deletar(int id);
+        List<CorEmpresa> Listar();
+    }
+}
