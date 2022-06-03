eead0f10566e025303620b296a64a9a44373269e
Quatrochifh <quatrochifabio145@gmail.com>
2022-06-03 17:16:19 -0300

    acabando... :(

--- RojoMobileE/RojoMobile/src/screens/main.js
+++ RojoMobileE/RojoMobile/src/screens/main.js
@@ -69,8 +69,8 @@ export default function Eventos() {
             </View>
             <View style={styles.MenuCont}>
                 <ScrollView style={styles.BolinhaAlinhada}>
-                    <View style={styles.cobrindoimaEvt}>
-                        <TouchableOpacity onPress={() => navigation.navigate('Evento')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Evento')}>
+                        <View style={styles.cobrindoimaEvt}>
                             <View style={styles.con}>
                                 <View style={styles.contTxt}>
                                     <Text style={styles.Txt}>EVENTOS</Text>
@@ -79,20 +79,20 @@ export default function Eventos() {
                                     <ImageBackground style={styles.BolinhaEvt} source={require('../../assets/eventosimg.png')} />
                                 </View>
                             </View>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.cobrindoimaCal}>
-                        <TouchableOpacity onPress={() => navigation.navigate('Calendario')}>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Calendario')}>
+                        <View style={styles.cobrindoimaCal}>
                             <Text style={styles.Txt}>CALENDARIO</Text>
                             <Image style={styles.BolinhaAgd} source={require('../../assets/agendaimg.png')} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.cobrindoimaDoc}>
-                        <TouchableOpacity onPress={() => navigation.navigate('Documento')}>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Documento')}>
+                        <View style={styles.cobrindoimaDoc}>
                             <Text style={styles.Txt}>DOCUMENTO</Text>
                             <Image style={styles.BolinhaDoc} source={require('../../assets/documentoimg.png')} />
-                        </TouchableOpacity>
-                    </View>
+                        </View>
+                    </TouchableOpacity>
                 </ScrollView>
             </View>
         </View>
@@ -103,12 +103,13 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         // flexDirection:"column",
-        backgroundColor: '#B01425'
+        backgroundColor: '#2e2829'
     },
     Headermain: {
         // display: "flex",
-        backgroundColor: '#fff',
-        borderBottomColor: '#FFF',
+        backgroundColor: '#2E2829',
+       // backgroundColor: '#fff',
+        borderBottomColor: '#2E2829',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -160,16 +161,16 @@ const styles = StyleSheet.create({
     cobrindoimaEvt: {
         borderRadius: 13,
         display: "flex",
-        justifyContent: "space-between",
-        flexDirection: "row",
+        justifyContent: "space-around",
+        // flexDirection: "row",
         backgroundColor: '#fff',
         marginBottom: 40,
         marginLeft: 30,
         width: 300,
         height: 150,
-        paddingLeft: 100,
-        paddingRight: 30,
-        backgroundColor: "#710B0B"
+        // paddingLeft: 100,
+        // paddingRight: 30,
+        backgroundColor: "#AA1200",
     },
     cobrindoimaCal: {
         borderRadius: 13,
@@ -178,9 +179,13 @@ const styles = StyleSheet.create({
         marginLeft: 30,
         width: 300,
         height: 150,
-        paddingLeft: 100,
-        paddingRight: 30,
-        backgroundColor: '#A13D3D'
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        // paddingLeft: 100,
+        // paddingRight: 30,
+        backgroundColor: "#AA1200",
     },
     cobrindoimaDoc: {
         borderRadius: 13,
@@ -189,20 +194,34 @@ const styles = StyleSheet.create({
         marginLeft: 30,
         width: 300,
         height: 150,
-        paddingLeft: 100,
-        paddingRight: 30,
-        backgroundColor: '#075668'
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        // paddingLeft: 100,
+        // paddingRight: 30,
+        backgroundColor: "#AA1200",
     },
     Txt: {
         color: '#FFF',
         marginTop: 30
     },
     contTxt: {
-        backgroundColor: "blue",
-        width: 90
+        // backgroundColor: "blue",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: 90,
+        height: 50,
+        marginLeft: 12
     },
     con: {
+        width: 300,
+        height: 150,
         display: "flex",
-        justifyContent: "space-between"
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        // backgroundColor: "blue"
     }
 })
